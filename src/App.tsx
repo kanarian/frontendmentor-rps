013874b1512de377e1eaf13b7c30b3d40c105a3c
kanarian <arian.joyandeh@hotmail.com>
2022-08-24 22:32:08 +0200

    Started working on header

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ReactComponent as Logo } from './images/logo.svg';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="flex flex-col content-center items-center justify-center bg-gray-700">
+      <header className=" flex w-3/5 h-32 rounded-md border-headerOutline border-4 content-evenly justify-between">
+        <Logo/>
+        <div className="bg-slate-300 rounded-md m-2 p-3">
+          <div className="text-score">
+            Score:
+          </div>
+          <div className="text-2xl text-dark">
+            800
+          </div>
+        </div>
       </header>
+      <section>
+        <div>
+          Main thing
+        </div>
+      </section>
+      <section>
+        <div>
+          Rules
+        </div>
+      </section>
     </div>
   );
 }
